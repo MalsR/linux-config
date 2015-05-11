f5589a6531f2adf916fce6f6381ec43ac84ebf8d
MalsR <malinda_rajapakse@hotmail.com>
2015-05-11 21:48:00 +0100

    Adding shell script to automate installing intellij, java

---
+++ install_java.sh
@@ -0,0 +1,15 @@
+~!/bin/bash
+
+tar -xvf jdk-7u79-linux-x64.tar.gz
+
+sudo mkdir /usr/lib/jvm
+
+sudo mv ./jdk1.7* /usr/lib/jvm/jdk1.7.0
+
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0/bin/javaws" 1
+
+sudo chmod a+x /usr/bin/java
+sudo chmod a+x /usr/bin/javac
+sudo chmod a+x /usr/bin/javaws
