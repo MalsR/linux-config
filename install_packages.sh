48f4352f5aab009ed33e110149171287a44c4151
MalsR <malinda_rajapakse@hotmail.com>
2015-05-13 21:48:24 +0100

    Added install pacakges shell

---
+++ install_packages.sh
@@ -0,0 +1,17 @@
+~!/bin/bash
+
+clear
+
+echo "Installing VIM..."
+
+sudo apt-get install vim
+
+echo "Installing latest GIT..."
+sudo apt-get install git
+
+#Installing Intellij
+echo "Installing Intellij Community Edition....................."
+sudo tar xfz ideaIC-14.1.2.tar.gz
+#cd ideaI*/
+#./idea.sh
+#cd ../
