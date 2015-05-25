a8e4de230ef3c4c08ed920c1dce49ea4e187aa18
MalsR <malinda_rajapakse@hotmail.com>
2015-05-25 20:14:57 +0100

    Added wget to packages list

--- install_packages.sh
+++ install_packages.sh
@@ -3,15 +3,16 @@
 clear
 
 echo "Installing VIM..."
-
 sudo apt-get install vim
 
 echo "Installing latest GIT..."
 sudo apt-get install git
 
-#Installing Intellij
-echo "Installing Intellij Community Edition....................."
-sudo tar xfz ideaIC-14.1.2.tar.gz
+echo "Installing wget"
+sudo apt-get install wget
+
+#can wget - echo "Installing Intellij Community Edition....................."
+#sudo tar xfz ideaIC-14.1.2.tar.gz
 #cd ideaI*/
 #./idea.sh
 #cd ../
