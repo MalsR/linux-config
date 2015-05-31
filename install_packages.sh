782af4a8d501a1b7be6c59da6dc109a32dc2f198
Malinda Rajapakse <malinda_rajapakse@hotmail.com>
2015-05-31 20:23:58 +0100

    Removed git and wget, no point

--- install_packages.sh
+++ install_packages.sh
@@ -8,17 +8,11 @@ clear
 echo "Installing VIM..."
 sudo apt-get install vim
 
-echo "Installing latest GIT..."
-sudo apt-get install git
-
-echo "Installing wget"
-sudo apt-get install wget
+echo "Installing tig..."
+sudo apt-get install tig
 
 #can wget - echo "Installing Intellij Community Edition....................."
 #sudo tar xfz ideaIC-14.1.2.tar.gz
 #cd ideaI*/
 #./idea.sh
 #cd ../
-
-echo "Installing tig"
-sudo apt-get install tig
