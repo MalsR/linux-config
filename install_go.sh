3b408205b97bb1688845960d28dac49278c3d8f5
MalsR <malinda_rajapakse@hotmail.com>
2017-01-11 21:51:43 +0000

    Add GOPATH env variable to go install script

--- install_go.sh
+++ install_go.sh
@@ -8,4 +8,10 @@ sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
 echo "Add to Path"
 export PATH=$PATH:/usr/local/go/bin
 
+echo "Create go path"
+cd projects-git
+mkdir gowork
+export GOPATH=$HOME/projects-git/gowork/
+export PATH=$PATH:$GOPATH/bin
+
 go version
