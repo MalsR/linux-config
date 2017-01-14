01518aaa20fdaf0e31c4e00ad0470d7fa14849ad
MalsR <malinda_rajapakse@hotmail.com>
2017-01-14 17:32:18 +0000

    Create src, pkg and bin folders
    Create necessary folder structure for go with src, bin and pkg.

--- install_go.sh
+++ install_go.sh
@@ -8,9 +8,13 @@ sudo tar -C /usr/local -xzf go1.7.4.linux-amd64.tar.gz
 
 echo "Create workspace folder"
 # Go path already in .bashrc
+# Explanation of go workspace see https://golang.org/doc/code.html
+
 mkdir gowork
 cd gowork
+
 mkdir src
+mkdir src/github.com/malsr
 mkdir bin
 mkdir pkg
 cd ../
