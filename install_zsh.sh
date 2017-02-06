8aa5bcd7372433f7d11cb80bddf4f46699eea72a
MalsR <malinda_rajapakse@hotmail.com>
2017-02-06 21:48:44 +0000

    Add ohmyzsh

--- install_zsh.sh
+++ install_zsh.sh
@@ -6,3 +6,5 @@ sudo apt-get install zsh
 echo "Check if zsh has installed succesfully"
 zsh --version
 cat /etc/shells
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
