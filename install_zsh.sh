d84983c192bda9d13e42e1abfb2460fb468dfe59
MalsR <malinda_rajapakse@hotmail.com>
2017-03-23 21:59:35 +0000

    Add copy of zshrc config

--- install_zsh.sh
+++ install_zsh.sh
@@ -7,4 +7,5 @@ echo "Check if zsh has installed succesfully"
 zsh --version
 cat /etc/shells
 
+# https://github.com/robbyrussell/oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
