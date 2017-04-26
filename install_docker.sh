fa122701d29f1958d8ef42d69e5755855c054f6f
MalsR <malinda_rajapakse@hotmail.com>
2017-04-26 21:21:15 +0100

    Add docker installation

--- install_docker.sh
+++ install_docker.sh
@@ -1,1 +1,21 @@
+~!/bin/bash
+
 # https://docs.docker.com/engine/reference/commandline/build/#build-with-path
+
+##################################
+## Install docker
+## | sh pipe to stdin
+##################################
+cd
+wget -qO- https://get.docker.com/ | sh
+
+##################################
+## give docker root access
+## add user to docker group
+##################################
+sudo usermod -aG docker $(whoami)
+
+
+docker version
+docker info
+docker images
